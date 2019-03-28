d59938cb2ff559f42e265a0ff5eff108b1350821
ŠKALDA Daniel <daniel.skalda@cns.cz>
2019-03-28 10:27:38 +0100

    Add new quote;

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <div class="container">
+        <app-new-quote @quoteAdded="newQuote"></app-new-quote>
         <app-quote-grid :quotes="quotes"></app-quote-grid>
     </div>
 </template>
 
 <script>
     import QuoteGrid from './components/QuoteGrid.vue';
+    import NewQuote from './components/NewQuote.vue';
+
     export default {
         data: () => {
             return {
@@ -15,8 +18,14 @@
                 maxQuotes: 10
             }
         },
+        methods: {
+            newQuote(quote) {
+                this.quotes.push(quote);    
+            }
+        },
         components: {
-            appQuoteGrid: QuoteGrid
+            appQuoteGrid: QuoteGrid,
+            appNewQuote: NewQuote
         }
     }
 </script>
