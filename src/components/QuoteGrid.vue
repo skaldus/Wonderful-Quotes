4e7d51a29c448cd8c929d33731970b01f9da8056
ŠKALDA Daniel <daniel.skalda@cns.cz>
2019-03-28 11:21:32 +0100

    Finish project;

--- src/components/QuoteGrid.vue
+++ src/components/QuoteGrid.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="row">
-        <app-quote v-for="quote in quotes"> {{ quote }}</app-quote>
+        <app-quote v-for="(quote, index) in quotes" @click.native="deleteQuote(index)"> {{ quote }}</app-quote>
     </div>
 </template>
 
@@ -11,7 +11,12 @@
         props: ['quotes'],
         components: {
             appQuote: Quote
-        } 
+        },
+        methods: {
+            deleteQuote(index) {
+                this.$emit('quoteDeleted', index);
+            }
+        }
     }
 </script>
 
